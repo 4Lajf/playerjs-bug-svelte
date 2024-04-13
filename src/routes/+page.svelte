a7a6dc21be4a7c3852eae044b4e271f92e5ac2b8
4Lajf <sajmon120xd@gmail.com>
2024-04-13 18:16:04 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { onMount } from 'svelte';
+	import { Playerjs } from './playerjs';
+	let player;
+
+	onMount(() => {
+		player = new Playerjs({
+			id: 'player',
+			file: `https://pixeldrain.com/api/file/7X1Gvu3B`,
+			poster: 'https://pixeldrain.com/api/file/FUN6giYi',
+			title: `test video`,
+			default_quality: '1080p',
+			thumbnails: 'https://pixeldrain.com/api/file/mdmPa81q'
+		});
+	});
+</script>
+
+	<div id="player" />
+
+<style>
+	@font-face {
+		font-family: 'vt323';
+		src: url('vt323-regular-webfont.woff2') format('woff2');
+		font-weight: normal;
+		font-style: normal;
+	}
+</style>
