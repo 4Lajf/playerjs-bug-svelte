446c348e3d26a7eb656cc4ea96b655bb93f47f18
4Lajf <sajmon120xd@gmail.com>
2024-04-13 18:38:21 +0200

    new bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,8 @@
 			poster: 'https://pixeldrain.com/api/file/FUN6giYi',
 			title: `test video`,
 			default_quality: '1080p',
-			thumbnails: 'https://pixeldrain.com/api/file/mdmPa81q'
+			thumbnails: 'https://pixeldrain.com/api/file/mdmPa81q',
+			points: [{ time: 12 }, { time: 33 }],
 		});
 	});
 </script>
