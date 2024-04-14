03a8996b6a8612617fdc96ac4f94ca44180241e4
4Lajf <sajmon120xd@gmail.com>
2024-04-14 02:33:05 +0200

    subtitle octopus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,36 @@
 <script>
 	import { onMount } from 'svelte';
 	import { Playerjs } from './playerjs';
+	import SubtitleOctopus from 'libass-wasm';
 	let player;
+	let subtitleOctopus;
 
 	onMount(() => {
 		player = new Playerjs({
 			id: 'player',
 			file: `https://pixeldrain.com/api/file/7X1Gvu3B`,
-			poster: 'https://pixeldrain.com/api/file/FUN6giYi',
 			title: `test video`,
 			default_quality: '1080p',
-			thumbnails: 'https://pixeldrain.com/api/file/mdmPa81q',
-			points: [{ time: 12 }, { time: 33 }],
+			thumbnails: 'https://pixeldrain.com/api/file/ZizkyAZB',
+			points:[{time:10, width:"90s", opacity:1, color:"#000000", text:"Description"},{time:33}]
 		});
+		document.getElementById('player').addEventListener('init', playerInit);
 	});
+
+	function playerInit() {
+		let video = document.querySelector(`[src="https://pixeldrain.com/api/file/7X1Gvu3B"]`);
+		console.log(video)
+		var options = {
+			video: video, // HTML5 video element
+			subUrl: 'https://pixeldrain.com/api/file/vKALavtx', // Link to subtitles
+			workerUrl: '/subtitles-octopus-worker.js', // Link to WebAssembly-based file "libassjs-worker.js"
+			legacyWorkerUrl: '/subtitles-octopus-worker-legacy.js',
+			debug: true,
+			renderMode: 'wasm-blend',
+			fallbackFont: 'https://pixeldrain.com/api/file/ghmx7kuj'
+		};
+		subtitleOctopus = new SubtitleOctopus(options);
+	}
 </script>
 
 	<div id="player" />
