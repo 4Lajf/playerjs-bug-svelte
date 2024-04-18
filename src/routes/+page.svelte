6391dba3e76aef36731b0ee63f82d0e30393032e
4Lajf <sajmon120xd@gmail.com>
2024-04-19 01:57:55 +0200

    skin change bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,26 @@
 <script>
 	import { onMount } from 'svelte';
 	import { Playerjs } from './playerjs';
-	import SubtitleOctopus from 'libass-wasm';
 	let player;
-	let subtitleOctopus;
-
 	onMount(() => {
 		player = new Playerjs({
 			id: 'player',
 			file: `https://pixeldrain.com/api/file/7X1Gvu3B`,
 			title: `test video`,
-			default_quality: '1080p',
-			thumbnails: 'https://pixeldrain.com/api/file/ZizkyAZB',
-			points:[{time:10, width:"90s", opacity:1, color:"#000000", text:"Description"},{time:33}],
-			rec: [
-				{
-					title: 'Metallic Rouge - 03',
-					poster: 'https://pixeldrain.com/api/file/Q5qJr1HK',
-					link: 'http://localhost:5173/anime/Metallic_Rouge/3/watch'
-				},
-				{
-					title: 'Metallic Rouge - 04',
-					poster: 'https://pixeldrain.com/api/file/KeZXEWXa',
-					link: 'http://localhost:5173/anime/Metallic_Rouge/4/watch'
-				}
-			]
 		});
-		document.getElementById('player').addEventListener('init', playerInit);
+
+
+		window.PlayerjsMenu2 = function (id, title, item) {
+			if (item === 0) {
+				pljssglobal[0].api('design', 1);
+			}
+
+			if (item === 1) {
+				pljssglobal[0].api('design', 2);
+			}
+		}; 
 	});
 
-	function playerInit() {
-		let video = document.querySelector(`[src="https://pixeldrain.com/api/file/7X1Gvu3B"]`);
-		console.log(video)
-		var options = {
-			video: video, // HTML5 video element
-			subUrl: 'https://pixeldrain.com/api/file/vKALavtx', // Link to subtitles
-			workerUrl: '/subtitles-octopus-worker.js', // Link to WebAssembly-based file "libassjs-worker.js"
-			legacyWorkerUrl: '/subtitles-octopus-worker-legacy.js',
-			debug: true,
-			renderMode: 'wasm-blend',
-			fallbackFont: 'https://pixeldrain.com/api/file/ghmx7kuj'
-		};
-		subtitleOctopus = new SubtitleOctopus(options);
-	}
 </script>
 
 	<div id="player" />
